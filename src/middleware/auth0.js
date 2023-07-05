8d926565cdad345ac0a15e9aff48fc96b96c93c8
Marek Balvín <marek267balvin@gmail.com>
2023-07-05 23:02:29 +0200

    Made aditional changes

--- src/middleware/auth0.js
+++ src/middleware/auth0.js
@@ -1,7 +1,7 @@
 const { auth } = require("express-oauth2-jwt-bearer");
 
 const validateAccessToken = auth({
-  audience: "https://primal-force-api-production.up.railway.app/",
+  audience: "https://primal-force-api.onrender.com/",
   issuerBaseURL: "https://dev-wdjg5l36.us.auth0.com/",
   tokenSigningAlg: "RS256",
 });
