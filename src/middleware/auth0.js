6fed7d22cb0c6fe2faf1d1babcfdb4473b95a589
Marek Balvín <marek267balvin@gmail.com>
2023-07-10 15:53:54 +0200

    Changed auth api

--- src/middleware/auth0.js
+++ src/middleware/auth0.js
@@ -1,7 +1,7 @@
 const { auth } = require("express-oauth2-jwt-bearer");
 
 const validateAccessToken = auth({
-  audience: "https://primal-force-api.onrender.com/",
+  audience: "https://primal-force-5293a17051c5.herokuapp.com/",
   issuerBaseURL: "https://dev-wdjg5l36.us.auth0.com/",
   tokenSigningAlg: "RS256",
 });
