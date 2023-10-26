6aa73e407ce31f489b5aa277f5d4e33e3d2da352
Marek Balvín <marek267balvin@gmail.com>
2023-10-26 17:47:51 +0200

    Added scope validation to controller

--- src/controller/match-controller.js
+++ src/controller/match-controller.js
@@ -5,11 +5,12 @@ const {
   getMatch,
   listMatches,
 } = require("../abl/match-abl");
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
 const router = express.Router();
 
-router.post("/add", addMatch);
-router.get("/get", getMatch);
-router.get("/list", listMatches);
-router.post("/delete", deleteMatch);
+router.post("/add", requiredScopes("create:match read:match"), addMatch);
+router.get("/get", requiredScopes("read:match"), getMatch);
+router.get("/list", requiredScopes("read:match"), listMatches);
+router.post("/delete", requiredScopes("delete:match"), deleteMatch);
 
 module.exports = router;
